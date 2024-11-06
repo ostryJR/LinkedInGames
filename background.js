fe20b5b58c9ecc280d79032d479632a2b66167dd
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2024-11-06 14:51:43 +0100

    working version

--- background.js
+++ background.js
@@ -5,13 +5,31 @@
 // Description: LinkedIn Games Extension
 // License: MIT
 
-var i = 0;
-console.log("Linked In Games Extension is running");
-// while(i<6){
-//     setTimeout(function(){
-//         var inputs = document.getElementsByTagName("input")[0];//.value = "MikiOst"; // Enter your MitID login here
-//         console.log(inputs);
-//         i = i+1;
-//     }, 500*i);
-// }
-// console.log("MitID login entered");
+
+
+var elementTango = `<li class="pr-game-hub__card">
+                  
+        <a href="/games/tango/" id="ember44" class="ember-view link-without-hover-state artdeco-card ps-game-entrypoint-card">
+          <div class="ps-game-entrypoint-card__insight-wrapper">
+            <div>
+<!---->
+              <span class="t-12 t-black--light t-normal inline-block full-width pt1 ps-game-entrypoint-card__insight">Harmonize the grid.
+              </span>
+            </div>
+
+            <span class="t-20 t-bold t-black full-width block truncate">Tango<span class="t-20
+                  ml1 t-black t-normal">#000</span>
+            </span>
+          </div>
+
+          <div class="ps-game-entry-point__bg-tango">
+            <img alt="Tango" src="https://static.licdn.com/aero-v1/sc/h/gqavqd9gk6ja1s27lr1p2zz2" class="ps-game-entrypoint-card__bg-img">
+          </div>
+
+        </a>
+  
+                </li>`;
+setTimeout(function(){
+    var games = document.getElementsByClassName('pr-game-hub__list')[0].innerHTML;
+    document.getElementsByClassName('pr-game-hub__list')[0].innerHTML = games + elementTango;
+}, 1000);
